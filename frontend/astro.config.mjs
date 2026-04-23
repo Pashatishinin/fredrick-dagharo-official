c6f5cd27b335e364a982067217a9ab240d75bad7
Pavlo Tishynin <pashatishinin@gmail.com>
2026-04-23 21:20:15 +0200

    vite

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,38 +1,38 @@
 // @ts-check
 
+import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import sanity from "@sanity/astro";
 import { loadEnv } from "vite";
 
-const {
-  PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_DATASET,
-  PUBLIC_SANITY_API_VERSION,
-} = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET, PUBLIC_SANITY_API_VERSION } = loadEnv(
+	process.env.NODE_ENV || "development",
+	process.cwd(),
+	"",
+);
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    server: {
-      fs: {
-        allow: [".."],
-      },
-    },
-    plugins: [tailwindcss()],
-    ssr: {
-      noExternal: ["gsap"],
-    },
-    optimizeDeps: {
-      include: ["gsap", "gsap/ScrollTrigger"],
-    },
-  },
-  integrations: [
-    sanity({
-      projectId: PUBLIC_SANITY_PROJECT_ID,
-      dataset: PUBLIC_SANITY_DATASET,
-      useCdn: false,
-      apiVersion: PUBLIC_SANITY_API_VERSION || "2024-03-27",
-    }),
-  ],
+	vite: {
+		server: {
+			fs: {
+				allow: [".."],
+			},
+		},
+		plugins: [tailwindcss()],
+		ssr: {
+			noExternal: ["gsap"],
+		},
+		optimizeDeps: {
+			include: ["gsap", "gsap/ScrollTrigger"],
+		},
+	},
+	integrations: [
+		sanity({
+			projectId: PUBLIC_SANITY_PROJECT_ID,
+			dataset: PUBLIC_SANITY_DATASET,
+			useCdn: false,
+			apiVersion: PUBLIC_SANITY_API_VERSION || "2024-03-27",
+		}),
+	],
 });
